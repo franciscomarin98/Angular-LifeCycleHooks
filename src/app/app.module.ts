e51019e0ed4542ad35596c94a5bfd9df5d50df34
francisco-1998 <francmarinc@gmail.com>
2021-02-02 09:27:16 -0500

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    MuestraNombreComponent
   ],
   imports: [
     BrowserModule
